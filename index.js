980237826a45b0d0b6d918f7fe9ee210599826c8
FruitieX <fruitiex@gmail.com>
2016-02-08 03:34:48 +0200

    WIP: multiple strips

--- index.js
+++ index.js
@@ -1,109 +1,133 @@
 'use strict';
 
-var numLeds = 91;
-var baudrate = 1000000; // 32u4 is FAST
 var port = 9009;
+var listenAddr = 'localhost';
+
+var strips = [
+    {
+        numLeds: 91,
+        name: 'desk',
+        dev: '/dev/ttyACM0',
+        baudrate: 1000000, // 32u4 is FAST
+        reversed: true,
+        header: new Buffer(6),
+        colors: []
+    },
+    {
+        numLeds: 11,
+        name: 'monitor',
+        dev: '/dev/ttyUSB1',
+        baudrate: 115200, // 328 not so much
+        reversed: true,
+        header: new Buffer(6),
+        colors: []
+    }
+];
+
+var readline = require('readline');
+var SerialPort = require('serialport').SerialPort;
+
+// initialize strips
+strips.forEach(function(strip) {
+    // start with all LEDs on
+    strip.colors = new Array(strip.numLeds).map(function() {
+        return {
+            red: 255,
+            green: 255,
+            blue: 255
+        };
+    });
+
+    // Adalight protocol header
+    strip.header.write('Ada', 0, 3, 'ascii');
+    // 4th and 5th bytes are high and low bytes respectively of (numLeds - 1)
+    strip.header.writeUInt16BE(strip.numLeds - 1, 3);
+    // 6th byte is XOR of (numLeds - 1) high, low bytes, then the constant 0x55
+    strip.header.writeUInt8(strip.header[3] ^ strip.header[4] ^ 0x55, 5);
+
+    strip.writeColor = function() {
+        var buf = new Buffer(6 + strip.numLeds * 3);
+        strip.header.copy(buf);
+
+        for (var i = 0; i < strip.numLeds; i++) {
+            if (!strip.colors[i]) {
+                continue;
+            }
+            buf[6 + (i * 3) + 0] = Math.round(strip.colors[i].red || 0);
+            buf[6 + (i * 3) + 1] = Math.round(strip.colors[i].green || 0);
+            buf[6 + (i * 3) + 2] = Math.round(strip.colors[i].blue || 0);
+        }
+
+        strip.serialPort.write(buf, function() {
+            strip.serialPort.drain(function() {
+                process.nextTick(strip.writeColor);
+            });
+        });
+    };
+
+    strip.openSerialPort = function(err) {
+        if (strip.serialPort) {
+            strip.serialPort.close(function() {
+                strip.serialPort = null;
+                setTimeout(strip.openSerialPort, 1000);
+            });
+        } else {
+            strip.serialPort = new SerialPort(strip.dev, {
+                baudrate: strip.baudrate,
+                dataBits : 8,
+                parity : 'none',
+                stopBits: 1,
+                flowControl : false
+            }, false);
+
+            strip.serialPort.on('error', function(err) {
+                setTimeout(function() {
+                    console.log(strip.name + ' serial port error: ' + err);
+                    console.log(strip.name + ' retrying in 1000 ms...');
+                    strip.openSerialPort();
+                }, 1000);
+            });
+            strip.serialPort.open(function(err) {
+                if (err) {
+                    console.log(strip.name + ' failed to open serial: ' + err);
+                    console.log(strip.name + ' retrying in 1000 ms...');
+                    strip.openSerialPort();
+                } else {
+                    strip.lineReader = readline.createInterface({
+                        input: strip.serialPort
+                    });
+                    strip.lineReader.on('line', function(line) {
+                        if (line === 'Ada') {
+                            console.log(strip.name + ' got ACK string');
+                            strip.writeColor();
+                        } else {
+                            console.log(strip.name + ' ' + line);
+                        }
+                    });
+                    console.log(strip.name + ' serial opened');
+                }
+            });
+        }
+    };
+
+    strip.openSerialPort();
+});
 
 var http = require('http');
 var server = http.createServer();
 var io = require('socket.io')(server);
-server.listen(port, 'localhost');
+server.listen(port, listenAddr);
 
 console.log('rgbd socket.io server listening on port 9009');
 
-var SerialPort = require('serialport').SerialPort;
-
-// Adalight protocol header
-var header = new Buffer(6);
-header.write('Ada', 0, 3, 'ascii');
-header.writeUInt16BE(numLeds - 1, 3); // number of leds - 1
-header.writeUInt8(header[3] ^ header[4] ^ 0x55, 5); // xor of numLeds high, low bytes, then 0x55
-
-var colors = [];
-for (var i = 0; i < numLeds; i++) {
-    colors[i] = {
-        red: 255,
-        green: 255,
-        blue: 255
-    };
-}
-
 io.on('connection', function(socket) {
     socket.on('frame', function(frame) {
-        colors = frame;
-    });
-    socket.on('led', function(data) {
-        colors[data.led] = data.colors;
+        if (!strips[frame.id]) {
+            console.log('invalid strip id: ' + frame.id);
+            socket.emit('error', 'invalid strip id: ' + frame.id +
+                                 ', max is: ' + strips.length - 1);
+        }
+
+        strips[frame.id].colors = frame.colors;
     });
 });
-
-var writeColor = function() {
-    var buf = new Buffer(6 + numLeds * 3);
-    header.copy(buf);
-
-    for (var i = 0; i < numLeds; i++) {
-        if (!colors[i]) {
-            continue;
-        }
-        buf[6 + (i * 3) + 0] = Math.round(colors[i].red || 0);
-        buf[6 + (i * 3) + 1] = Math.round(colors[i].green || 0);
-        buf[6 + (i * 3) + 2] = Math.round(colors[i].blue || 0);
-    }
-
-    serialPort.write(buf, function() {
-        serialPort.drain(function() {
-            process.nextTick(writeColor);
-        });
-    });
-};
-
-var readline = require('readline');
-var lineReader;
-
-var serialPort = null;
-var openSerialPort = function(err) {
-    if (serialPort) {
-        serialPort.close(function() {
-            serialPort = null;
-            setTimeout(openSerialPort, 1000);
-        });
-    } else {
-        serialPort = new SerialPort('/dev/ttyACM0', {
-            baudrate: baudrate,
-            dataBits : 8,
-            parity : 'none',
-            stopBits: 1,
-            flowControl : false
-        }, false);
-
-        serialPort.on('error', function(err) {
-            setTimeout(function() {
-                console.log('serial port error: ' + err);
-                console.log('retrying in 1000 ms...');
-                openSerialPort();
-            }, 1000);
-        });
-        serialPort.open(function(err) {
-            if (err) {
-                console.log('failed to open serial: ' + err);
-                console.log('retrying in 1000 ms...');
-                openSerialPort();
-            } else {
-                lineReader = readline.createInterface({
-                    input: serialPort
-                });
-                lineReader.on('line', function(line) {
-                    if (line === 'Ada') {
-                        console.log('got ACK string');
-                        writeColor();
-                    } else {
-                        console.log(line);
-                    }
-                });
-                console.log('serial opened');
-            }
-        });
-    }
-};
-
-openSerialPort();
