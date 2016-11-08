82035348a41d1f672a866a19cb5904357213be05
Rasmus Eskola <fruitiex@gmail.com>
2016-11-08 14:13:50 +0200

    Support registering multiple patterns and selecting between them

--- index.js
+++ index.js
@@ -1,60 +1,42 @@
 'use strict';
 
+const _ = require('lodash');
+const color = require('tinycolor2');
+
 var port = 9009;
 var listenAddr = '0.0.0.0';
+
+var patterns = {};
+var activePattern = null;
+var prevPattern = null;
+var fadeTime = new Date().getTime();
 
 var strips = [
     {
         numLeds: 90,
         name: 'desk',
-        dev: '/dev/ttyACM0',
+        dev: null,
         baudrate: 1000000, // 32u4 is FAST
         reversed: false,
         header: new Buffer(6),
         colors: [],
-        oldColors: [],
         fps: 0,
         fpsLastSample: new Date().getTime(),
         brightness: 1
     }
-    /*
-    {
-        numLeds: 11,
-        name: 'monitor',
-        dev: '/dev/ttyUSB1',
-        baudrate: 38400, // 328 not so much
-        reversed: true,
-        header: new Buffer(6),
-        colors: [],
-        oldColors: [],
-        fps: 0,
-        fpsLastSample: new Date().getTime()
-    }
-    */
 ];
 
 var readline = require('readline');
 var SerialPort = require('serialport');
-
-var color = require('tinycolor2');
-
-var fadeTime = new Date().getTime();
 
 let fpsAvgFactor = 0.975;
 
 let socketListenerFramerate = 20;
 
 // initialize strips
-strips.forEach(function(strip) {
+strips.forEach(function(strip, index) {
     // start with all LEDs off
     strip.colors = Array.from(Array(strip.numLeds)).map(function() {
-        return {
-            r: 0,
-            g: 0,
-            b: 0
-        };
-    });
-    strip.oldColors = Array.from(Array(strip.numLeds)).map(function() {
         return {
             r: 0,
             g: 0,
@@ -73,28 +55,60 @@ strips.forEach(function(strip) {
         var buf = new Buffer(6 + strip.numLeds * 3);
         strip.header.copy(buf);
 
+        const pattern = patterns[activePattern];
+        const oldPattern = patterns[prevPattern];
+
+        let patternStrip = null;
+        let oldPatternStrip = null;
+
+        if (!pattern || !pattern[index]) {
+            patternStrip = Array.from(Array(strip.numLeds)).map(function() {
+                return {
+                    r: 0,
+                    g: 0,
+                    b: 0
+                };
+            });
+        } else {
+            patternStrip = pattern[index];
+        }
+
+        if (!oldPattern || !oldPattern[index]) {
+            oldPatternStrip = Array.from(Array(strip.numLeds)).map(function() {
+                return {
+                    r: 0,
+                    g: 0,
+                    b: 0
+                };
+            });
+        } else {
+            oldPatternStrip = oldPattern[index];
+        }
+
         for (var i = 0; i < strip.numLeds; i++) {
-            if (!strip.colors[i]) {
-                continue;
-            }
-
             let fade = 100 - Math.min(100, (new Date().getTime() - fadeTime) / 25);
-            let c = null;
 
-            if (fade) {
-                c = color.mix(color(strip.colors[i]), color(strip.oldColors[i]), fade);
+            let c = null;
+            if (fade && oldPatternStrip) {
+                c = color.mix(color(patternStrip[i]), color(oldPatternStrip[i]), fade);
                 c = c.toRgb();
             } else {
-                c = strip.colors[i];
+                c = Object.assign({}, patternStrip[i]);
             }
 
-            let r = c.r * strip.brightness;
-            let g = c.g * strip.brightness;
-            let b = c.b * strip.brightness;
+            c.r *= strip.brightness;
+            c.g *= strip.brightness;
+            c.b *= strip.brightness;
 
-            buf[6 + (i * 3) + 0] = Math.round(r || 0);
-            buf[6 + (i * 3) + 1] = Math.round(g || 0);
-            buf[6 + (i * 3) + 2] = Math.round(b || 0);
+            strip.colors[i] = c;
+
+            buf[6 + (i * 3) + 0] = Math.round(c.r || 0);
+            buf[6 + (i * 3) + 1] = Math.round(c.g || 0);
+            buf[6 + (i * 3) + 2] = Math.round(c.b || 0);
+        }
+
+        if (!strip.dev) {
+            return;
         }
 
         strip.serialPort.write(buf, function() {
@@ -110,7 +124,7 @@ strips.forEach(function(strip) {
 
     strip.openSerialPort = function(err) {
         if (!strip.dev) {
-            return;
+            setInterval(strip.writeColor, 10);
         } else if (strip.serialPort) {
             strip.serialPort.close(function() {
                 strip.serialPort = null;
@@ -167,10 +181,9 @@ server.listen(port, listenAddr);
 console.log('rgbd socket.io server listening on port 9009');
 
 io.on('connection', function(socket) {
-    fadeTime = new Date().getTime();
-    strips.forEach(strip => {
-        strip.oldColors = strip.colors.slice(0);
-    });
+    // emit existing patterns right away
+    socket.emit('patterns', _.keys(patterns));
+    socket.emit('activate', activePattern);
 
     socket.on('frame', function(frame) {
         if (!strips[frame.id]) {
@@ -180,10 +193,31 @@ io.on('connection', function(socket) {
             return;
         }
 
-        strips[frame.id].colors = frame.colors;
-        if (strips[frame.id].reversed) {
-            strips[frame.id].colors.reverse();
+        if (!frame.name) {
+            console.log('must provide frame name!');
+            socket.emit('err', 'must provide frame name!');
+            return;
         }
+
+        // TODO clean up on disconnect
+        if (!patterns[frame.name]) {
+            patterns[frame.name] = [];
+            socket.broadcast.emit('patterns', _.keys(patterns));
+        }
+
+        patterns[frame.name][frame.id] = frame.colors;
+
+        if (strips[frame.id].reversed) {
+            patterns[frame.name][frame.id].reverse();
+        }
+    });
+
+    socket.on('activate', function(name) {
+        prevPattern = activePattern;
+        activePattern = name;
+        fadeTime = new Date().getTime();
+
+        socket.broadcast.emit('activate', name);
     });
 
     socket.on('stripsSubscribe', function() {
@@ -199,14 +233,13 @@ io.on('connection', function(socket) {
 });
 
 setInterval(() => {
-    io.to('strips').emit('strips', strips.map(strip => ({
-        numLeds: strip.numLeds,
-        name: strip.name,
-        dev: strip.dev,
-        baudrate: strip.baudrate,
-        reversed: strip.reversed,
-        colors: strip.colors,
-        fps: strip.fps,
-        brightness: strip.brightness
-    })));
+    let emittedStrips = strips.map(strip => {
+        let omitted = _.omit(strip, [ 'header' ]);
+
+        omitted.patterns = _.keys(patterns);
+
+        return omitted;
+    });
+
+    io.to('strips').emit('strips', emittedStrips);
 }, 1000 / socketListenerFramerate);
